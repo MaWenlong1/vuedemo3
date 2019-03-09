135040c5c5873f138e5cd4d30e86b0d71a247fc8
mawenlong <mwljndx@163.com>
2019-03-10 00:28:53 +0800

    结构完成

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,42 +1,106 @@
 <template>
   <div>
-    <div class="app-header">
+    <div class="app-head">
       <div class="app-head-inner">
-        <router-link :to="{path: '/'}">
-          <img src="../assets/logo.png">
+        <router-link :to="{ path: '/' }">
+          <img src="../assets/logo.png" />
         </router-link>
         <div class="head-nav">
           <ul class="nav-list">
-            <li>登录</li>
+            <li>{{ username }}</li>
+            <li v-if="username !== ''" class="nav-pile">|</li>
+            <li v-if="username !== ''" @click="quit">退出</li>
+            <li v-if="username === ''" @click="logClick">登录</li>
             <li class="nav-pile">|</li>
-            <li>注册</li>
-            <li class="nav-pile">|</li>
-            <li>关于</li>
+            <li v-if="username === ''" @click="regClick">注册</li>
+            <li v-if="username === ''" class="nav-pile">|</li>
+            <li @click="aboutClick">关于</li>
           </ul>
         </div>
       </div>
     </div>
-    <div class="app-content">
-      <router-view/>
+    <div class="container">
+      <keep-alive>
+        <router-view />
+      </keep-alive>
     </div>
-    <div class="app-footer">
-      <p> 2018 fishenal MIT</p>
+    <div class="app-foot">
+      <p>© 2016 fishenal MIT</p>
     </div>
+    <my-dialog
+      :isShow="isShowAboutDialog"
+      @on-close="closeDialog('isShowAboutDialog')"
+    >
+      <p>
+        本报告在调研数据的基础上，采用定性与定量相结合的方式深入分析了专车市场发展的驱动因素与阻碍因素、专车市场背后的产业格局、专车企业的竞争格局、用户对专车市场的依赖程度、专车对其他交通工具运力的补充效应等，通过这五个章节的研究反映专车市场的发展态势和面临的问题。报告力求客观、深入、准确地反映中国专车市场发展情况，为政府、企事业单位和社会各界提供决策依据。
+      </p>
+    </my-dialog>
+
+    <my-dialog
+      :isShow="isShowLogDialog"
+      @on-close="closeDialog('isShowLogDialog')"
+    >
+      <log-form @has-log="onSuccessLog"></log-form>
+    </my-dialog>
+    <my-dialog
+      :isShow="isShowRegDialog"
+      @on-close="closeDialog('isShowRegDialog')"
+    >
+      <reg-form></reg-form>
+    </my-dialog>
   </div>
 </template>
 
 <script>
+import MyDialog from "./base/dialog";
+import LogForm from "./logForm";
+import RegForm from "./regForm";
+
 export default {
+  components: {
+    MyDialog,
+    LogForm,
+    RegForm
+  },
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      isShowAboutDialog: false,
+      isShowLogDialog: false,
+      isShowRegDialog: false,
+      username: ""
     };
+  },
+  methods: {
+    logClick() {
+      this.isShowLogDialog = true;
+    },
+    regClick() {
+      this.isShowRegDialog = true;
+    },
+    aboutClick() {
+      this.isShowAboutDialog = true;
+    },
+    closeDialog(attr) {
+      this[attr] = false;
+    },
+    onSuccessLog(data) {
+      this.closeDialog("isShowLogDialog");
+      this.username = data.username;
+    },
+    quit() {
+      this.username = "";
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
 html,
 body,
 div,
---
+++ src/jsonserver/README.md
@@ -0,0 +1,11 @@
+### jsonserver搭建
+安装
+sudo npm install json-server -g
+
+复制.json文件到某个目录运行命令：
+json-server db.json -p 3003
+
+https://www.cnblogs.com/ys-wuhan/p/6387791.html
+
+自定义路由：https://blog.csdn.net/weixin_40817115/article/details/81281454
+json-server --routes route.json db.json
